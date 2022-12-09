d3f99c5e52606aea2cc30a2b89125fae61869987
Henry Tran <huy8691@gmail.com>
2022-12-09 16:27:05 +0700

    page account

--- pages/account/index.page.tsx
+++ pages/account/index.page.tsx
@@ -1,24 +1,34 @@
 import React, { useEffect, useState } from 'react'
 
+// next
+import Head from 'next/head'
+
 import type { ReactElement } from 'react'
 import NestedLayout from 'src/layout/nestedLayout'
 import type { NextPageWithLayout } from 'pages/_app.page'
 
-// import { useAppSelector } from 'src/store/hooks'
+import { useAppSelector } from 'src/store/hooks'
 // dayjs
-import { Dayjs } from 'dayjs'
+import dayjs, { Dayjs } from 'dayjs'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 // mui
 // import List from '@mui/material/List'
 // import ListItem from '@mui/material/ListItem'
 // import ListItemText from '@mui/material/ListItemText'
-import { Box, FormControl, FormHelperText } from '@mui/material'
+import {
+  Box,
+  FormControl,
+  FormHelperText,
+  Card,
+  CardContent,
+  Typography,
+} from '@mui/material'
 import {
   ButtonCustom,
   InputLabelCustom,
   MenuItemSelectCustom,
-  // PlaceholderSelect,
+  PlaceholderSelect,
   SelectCustom,
   TextFieldCustom,
 } from 'src/components'
@@ -26,11 +36,26 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 
 import { AccountDataType } from './accountModel'
 // mui
-
+import { styled } from '@mui/material/styles'
 import { useForm, Controller } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { schema } from './validations'
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
+
+// custom style
+const CardPage = styled(Card)(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'dark' ? theme.palette.action.hover : '#F8F9FC',
+  boxShadow: 'none',
+}))
+const CardContentCustom = styled(CardContent)(() => ({
+  paddingBottom: '16px !important',
+}))
+const TypographyH1 = styled(Typography)(({ theme }) => ({
+  fontSize: '2rem',
+  fontWeight: 'bold',
+  color: theme.palette.mode === 'dark' ? '#ddd' : '##49516F',
+}))
 
 const genderArray = [
   {
@@ -48,10 +73,9 @@ const genderArray = [
 ]
 
 const Account: NextPageWithLayout = () => {
-  // const userInfo = useAppSelector((state) => state.userInfo)
+  const userInfo = useAppSelector((state) => state.userInfo)
   // fix error when use next theme
   const [stateDate, setStateDate] = useState<Dayjs | null>(null)
-  const [mounted, setMounted] = useState(false)
   const {
     handleSubmit,
     control,
@@ -70,13 +94,12 @@ const Account: NextPageWithLayout = () => {
   }
 
   useEffect(() => {
-    setMounted(true)
-  }, [])
-  if (!mounted) {
-    return null
-  }
-
-  // fix error when use next theme
+    setValue('first_name', userInfo.data.first_name)
+    setValue('last_name', userInfo.data.last_name)
+    setValue('gender', userInfo.data.gender)
+    setValue('dob', dayjs(`${userInfo.data.dob}`))
+    setValue('address', userInfo.data.address)
+  }, [userInfo, setValue])
 
   return (
     // <Box
@@ -125,145 +148,181 @@ const Account: NextPageWithLayout = () => {
     //     </List>
     //   </Container>
     // </Box>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Box>
-        <Controller
-          control={control}
-          name="first_name"
-          defaultValue="hehe"
-          render={({ field }) => (
-            <>
-              <InputLabelCustom
-                htmlFor="first_name"
-                error={!!errors.first_name}
-              >
-                First name
-              </InputLabelCustom>
-              <FormControl fullWidth>
-                <TextFieldCustom
-                  id="first_name"
-                  error={!!errors.first_name}
-                  {...field}
-                />
-                <FormHelperText error={!!errors.first_name}>
-                  {errors.first_name && `${errors.first_name.message}`}
-                </FormHelperText>
-              </FormControl>
-            </>
-          )}
-        />
-      </Box>
-      <Box>
-        <Controller
-          control={control}
-          name="last_name"
-          render={({ field }) => (
-            <>
-              <InputLabelCustom htmlFor="last_name" error={!!errors.last_name}>
-                Last name
-              </InputLabelCustom>
-              <FormControl fullWidth>
-                <TextFieldCustom
-                  id="last_name"
-                  error={!!errors.last_name}
-                  {...field}
-                />
-                <FormHelperText error={!!errors.last_name}>
-                  {errors.last_name && `${errors.last_name.message}`}
-                </FormHelperText>
-              </FormControl>
-            </>
-          )}
-        />
-      </Box>
-      <Box>
-        <Controller
-          control={control}
-          name="gender"
-          render={({ field }) => (
-            <>
-              <InputLabelCustom htmlFor="gender" error={!!errors.gender}>
-                Gender
-              </InputLabelCustom>
-              <FormControl fullWidth>
-                <SelectCustom
-                  id="gender"
-                  displayEmpty
-                  defaultValue=""
-                  IconComponent={() => <KeyboardArrowDownIcon />}
-                  // renderValue={(value: any) => {
-                  //   if (value === '') {
-                  //     return (
-                  //       <PlaceholderSelect>
-                  //         <div>Select gender</div>
-                  //       </PlaceholderSelect>
-                  //     )
-                  //   }
-                  //   return genderArray?.find((obj) => obj.name === value)?.name
-                  // }}
-                  {...field}
-                  onChange={(event: any) => {
-                    setValue('gender', event.target.value)
-                  }}
-                >
-                  {genderArray?.map((item, index) => {
-                    return (
-                      <MenuItemSelectCustom
-                        value={item.name}
-                        key={index + Math.random()}
+    <>
+      <Head>
+        <title>User Information | Vape</title>
+      </Head>
+      <TypographyH1 variant="h1" mb={3}>
+        User Information
+      </TypographyH1>
+      <CardPage>
+        <CardContentCustom>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Box mb={2}>
+              <Controller
+                control={control}
+                name="first_name"
+                defaultValue="hehe"
+                render={({ field }) => (
+                  <>
+                    <InputLabelCustom
+                      htmlFor="first_name"
+                      error={!!errors.first_name}
+                    >
+                      First name
+                    </InputLabelCustom>
+                    <FormControl fullWidth>
+                      <TextFieldCustom
+                        id="first_name"
+                        error={!!errors.first_name}
+                        {...field}
+                      />
+                      <FormHelperText error={!!errors.first_name}>
+                        {errors.first_name && `${errors.first_name.message}`}
+                      </FormHelperText>
+                    </FormControl>
+                  </>
+                )}
+              />
+            </Box>
+            <Box mb={2}>
+              <Controller
+                control={control}
+                name="last_name"
+                render={({ field }) => (
+                  <>
+                    <InputLabelCustom
+                      htmlFor="last_name"
+                      error={!!errors.last_name}
+                    >
+                      Last name
+                    </InputLabelCustom>
+                    <FormControl fullWidth>
+                      <TextFieldCustom
+                        id="last_name"
+                        error={!!errors.last_name}
+                        {...field}
+                      />
+                      <FormHelperText error={!!errors.last_name}>
+                        {errors.last_name && `${errors.last_name.message}`}
+                      </FormHelperText>
+                    </FormControl>
+                  </>
+                )}
+              />
+            </Box>
+            <Box mb={2}>
+              <Controller
+                control={control}
+                name="gender"
+                render={({ field }) => (
+                  <>
+                    <InputLabelCustom htmlFor="gender" error={!!errors.gender}>
+                      Gender
+                    </InputLabelCustom>
+                    <FormControl fullWidth>
+                      <SelectCustom
+                        id="gender"
+                        displayEmpty
+                        IconComponent={() => <KeyboardArrowDownIcon />}
+                        renderValue={(value: any) => {
+                          if (value === '') {
+                            return (
+                              <PlaceholderSelect>
+                                <div>Select gender</div>
+                              </PlaceholderSelect>
+                            )
+                          }
+                          return genderArray?.find((obj) => obj.name === value)
+                            ?.name
+                        }}
+                        {...field}
+                        onChange={(event: any) => {
+                          setValue('gender', event.target.value)
+                        }}
                       >
-                        {item.name}
-                      </MenuItemSelectCustom>
-                    )
-                  })}
-                </SelectCustom>
-                <FormHelperText error={!!errors.gender}>
-                  {errors.gender && `${errors.gender.message}`}
-                </FormHelperText>
-              </FormControl>
-            </>
-          )}
-        />
-      </Box>
-      <Box>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker
-            value={stateDate}
-            onChange={(newStateDate) => {
-              setStateDate(newStateDate)
-              console.log(newStateDate)
-            }}
-            renderInput={(params) => <TextFieldCustom {...params} />}
-          />
-        </LocalizationProvider>
-      </Box>
-      <Box>
-        <Controller
-          control={control}
-          name="address"
-          render={({ field }) => (
-            <>
-              <InputLabelCustom htmlFor="address" error={!!errors.address}>
-                Address
-              </InputLabelCustom>
-              <FormControl fullWidth>
-                <TextFieldCustom
-                  id="address"
-                  error={!!errors.address}
-                  {...field}
-                />
-                <FormHelperText error={!!errors.address}>
-                  {errors.address && `${errors.address.message}`}
-                </FormHelperText>
-              </FormControl>
-            </>
-          )}
-        />
-      </Box>
-      <ButtonCustom variant="contained" size="large" type="submit">
-        Update Information
-      </ButtonCustom>
-    </form>
+                        {genderArray?.map((item, index) => {
+                          return (
+                            <MenuItemSelectCustom
+                              value={item.name}
+                              key={index + Math.random()}
+                            >
+                              {item.name}
+                            </MenuItemSelectCustom>
+                          )
+                        })}
+                      </SelectCustom>
+                      <FormHelperText error={!!errors.gender}>
+                        {errors.gender && `${errors.gender.message}`}
+                      </FormHelperText>
+                    </FormControl>
+                  </>
+                )}
+              />
+            </Box>
+            <Box mb={2}>
+              <Controller
+                control={control}
+                name="dob"
+                render={({ field }) => (
+                  <>
+                    <InputLabelCustom htmlFor="dob" error={!!errors.dob}>
+                      Dob
+                    </InputLabelCustom>
+                    <FormControl fullWidth>
+                      <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DatePicker
+                          {...field}
+                          onChange={(value: Dayjs) => {
+                            console.log('value', value)
+                            setValue('dob', new Date(value))
+                          }}
+                          renderInput={(params) => (
+                            <TextFieldCustom {...params} error={!!errors.dob} />
+                          )}
+                        />
+                      </LocalizationProvider>
+                      <FormHelperText error={!!errors.dob}>
+                        {errors.dob && `${errors.dob.message}`}
+                      </FormHelperText>
+                    </FormControl>
+                  </>
+                )}
+              />
+            </Box>
+            <Box mb={2}>
+              <Controller
+                control={control}
+                name="address"
+                render={({ field }) => (
+                  <>
+                    <InputLabelCustom
+                      htmlFor="address"
+                      error={!!errors.address}
+                    >
+                      Address
+                    </InputLabelCustom>
+                    <FormControl fullWidth>
+                      <TextFieldCustom
+                        id="address"
+                        error={!!errors.address}
+                        {...field}
+                      />
+                      <FormHelperText error={!!errors.address}>
+                        {errors.address && `${errors.address.message}`}
+                      </FormHelperText>
+                    </FormControl>
+                  </>
+                )}
+              />
+            </Box>
+            <ButtonCustom variant="contained" size="large" type="submit">
+              Update Information
+            </ButtonCustom>
+          </form>
+        </CardContentCustom>
+      </CardPage>
+    </>
   )
 }
 
